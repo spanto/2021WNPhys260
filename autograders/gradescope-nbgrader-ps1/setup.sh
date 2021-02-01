26c72724c31253d16ac3644c12f0f453d2f16a47
Eric Spanton <spanton2@gmail.com>
2021-02-01 13:37:22 -0500

    Updated to PF3
    Made very minor changes to PF1-PF3 and PS1-PS2

---
+++ autograders/gradescope-nbgrader-ps1/setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+apt-get install -y python3 python3-pip python3-dev jq
+
+pip3 install nbgrader numpy matplotlib
+# Update these back to the latest versions
+pip3 install jupyter-console ipython
+
+# This is for GradeScope
+pip3 install -r /autograder/source/requirements.txt
+
+cp /autograder/source/nbgrader_config.py /autograder
+cp /autograder/source/source/header.ipynb /autograder/
+cp -r /autograder/source/source/ps1 /autograder/
